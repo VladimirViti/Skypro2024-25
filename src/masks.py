2815464f10bf9ac1f50f61d5a417860cc6f6f8ad
Vladimir <vladimir.vition@inbox.ru>
2025-03-08 22:57:16 +0500

    redact masks 2

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,10 @@
 def get_mask_card_number(number_catr: int) -> str:
     """Функцию маскировки номера банковской карты"""
-    mask_number = "XXXX XX** **** XXXX"
     number_1 = str(number_catr)
-    return f"{number_1[:4]} {number_1[4:6]}{mask_number[7:-4]} {number_1[-4:]}"
+    return f"{number_1[0:-12]} {number_1[-12:-10]}** **** {number_1[-4:]}"
 
 
 def get_mask_account(get_accout: int) -> str:
     """Функцию маскировки номера банковского счета"""
-    mask_account = "**XXXX"
     get_accout_str = str(get_accout)
-    return f"{mask_account[:2]}{get_accout_str[-4:]}"
+    return f"{get_accout_str[:-20]}**{get_accout_str[-4:]}"
